b743fec0526409ae48fbf15a8bfdbf54542233b9
zozep <jpark041@gmail.com>
2019-09-04 10:01:08 -0700

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,5 +1,16 @@
 class StudentsController < ApplicationController
+  
   def index
     @students = Student.all
   end
+
+  def show
+    @student = set_student
+  end
+
+  private
+    def set_student
+      @student = Student.find(params[:id]).to_s
+    end
+
 end
